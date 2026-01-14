f9cfbe19e121c65f7c57e275a8cde0f41f7e893b
Nyro <nyro0319@gmail.com>
2026-01-14 14:46:47 +0100

    Update Ejercicio 148.py

--- EJERCICIOS DE LOS ALUMNOS/NUBIA/Curso_programación/Tuplas/Ejercicio 148.py
+++ EJERCICIOS DE LOS ALUMNOS/NUBIA/Curso_programación/Tuplas/Ejercicio 148.py
@@ -2,11 +2,11 @@
 # Convertir el contenido de la tupla a tipo lista. 
 # Modificar la lista y luego convertir la lista en tupla.
 
-tupla = (1, 2, 3)
-lista = list(tupla)
+tupla = (1, 2, 3) # Defino lista con 3 valores
+lista = list(tupla) # Convierto tupla a lista
 
-lista.append(4)
+lista.append(4) # Agrego valor a la lista (la modifico)
 
-tupla = tuple(lista)
+tupla = tuple(lista) # La vuelvo a convertir en tupla
 
-print(tupla)
+print(tupla) # Muestro en pantalla la tupla

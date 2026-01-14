1fd3568a9dbbd34b97fe341d22742f4260e0d79e
Mauri-74 <asarahlobo@gmail.com>
2026-01-14 13:59:29 +0100

    Ejercicios

---
+++ EJERCICIOS DE LOS ALUMNOS/MAURICIO/Dia_5/Dia5_Funciones_Ejercicio138.py
@@ -0,0 +1,12 @@
+# Funciones
+
+
+def titulo_subrayado(titulo, caracter="*"):
+    print(titulo)
+    print(caracter * len(titulo))
+
+
+# Bloque Principal
+
+titulo_subrayado("Sistema de Administracion")
+titulo_subrayado("Ventas", "-")

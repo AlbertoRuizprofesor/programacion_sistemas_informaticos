c5828406b46e9ef23bb3ebec190aec4352d8592c
Nyro <nyro0319@gmail.com>
2026-01-15 09:31:35 +0100

    Nuevos

---
+++ EJERCICIOS DE LOS ALUMNOS/NUBIA/Curso programación/Bibliotecas/Proyecto 2/Mayormenor.py
@@ -0,0 +1,13 @@
+
+def mayor():
+    nummayor = []
+    for x in range(2):
+        nummayor.append(int(input("Ingrese un número: ")))
+    return max(nummayor)
+
+def menor():
+    nummenor = []
+    for x in range(2):
+        nummenor.append(int(input("Ingrese un número: ")))
+    return min(nummenor)
+

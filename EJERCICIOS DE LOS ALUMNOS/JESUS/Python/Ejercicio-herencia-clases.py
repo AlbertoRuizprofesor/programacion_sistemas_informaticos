0b9bd3fc11ef45a826fa9d755a4bbefb830375ae
Jesus <jesus-dp@hotmail.es>
2026-01-16 13:58:47 +0100

    12123

--- EJERCICIOS DE LOS ALUMNOS/JESUS/Python/Ejercicio-herencia-clases.py
+++ EJERCICIOS DE LOS ALUMNOS/JESUS/Python/Ejercicio-herencia-clases.py
@@ -1,86 +1,86 @@
 # ejercicio de herencia 
 
 class Producto:
-    def __init__(self):
-        self.nombre=""
-        self.fabricante=""
-        self.precio=""
+    def __init__(self,nombre,fabricante,precio):
+        self.nombre=nombre
+        self.fabricante=fabricante
+        self.precio=precio
     
     #def impri(self):
         #print(self.nombre,self.fabricante,self.precio)
 
 class Electronica(Producto):
-    def __init__(self):
-        super().__init__()
+    def __init__(self,nombre,fabricante,precio):
+        super().__init__(nombre,fabricante,precio)
         
         self.modo="Gamming"
         #print(self.nombre,self.fabricante,self.precio,self.modo)
 
 class Electrodomestico(Producto):
-    def __init__(self):
-        super().__init__()
+    def __init__(self,nombre,fabricante,precio):
+        super().__init__(nombre,fabricante,precio)
         self.consumo="Wat"
 
 class Monitores(Electronica):
-    def __init__(self):
-        super().__init__()
-        self.nombre="rgergerg"
-        self.fabricante="Asus"
-        self.precio="200"
-        self.pulgada="21"
+    def __init__(self,nombre,fabricante,precio,pulgada):
+        super().__init__(nombre,fabricante,precio)
+        self.nombre=nombre
+        self.fabricante=fabricante
+        self.precio=precio
+        self.pulgada=pulgada
         print(self.nombre,self.fabricante,self.precio,self.modo,self.pulgada)
 
 class Teclado(Electronica):
-    def __init__(self):
-        super().__init__()
-        self.nombre="465wf"
-        self.fabricante="logitec"
-        self.precio="80"
-        self.ergonomico="ergonomico"
+    def __init__(self,nombre,fabricante,precio,ergonomico):
+        super().__init__(nombre,fabricante,precio)
+        self.nombre=nombre
+        self.fabricante=fabricante
+        self.precio=precio
+        self.ergonomico=ergonomico
         print(self.nombre,self.fabricante,self.precio,self.modo,self.ergonomico)
 
 class Portatil(Electronica):
-    def __init__(self):
-        super().__init__()
-        self.nombre="123dasdasd"
-        self.fabricante="HP"
-        self.precio="1888"
-        self.ram="32GB"
+    def __init__(self,nombre,fabricante,precio,ram):
+        super().__init__(nombre,fabricante,precio)
+        self.nombre=nombre
+        self.fabricante=fabricante
+        self.precio=precio
+        self.ram=ram
         print(self.nombre,self.fabricante,self.precio,self.modo,self.ram)
 
 class Lavadora(Electrodomestico):
-    def __init__(self):
-        super().__init__()
-        self.nombre="iqwjoqw"
-        self.fabricante="Daewoo"
-        self.precio="800"
-        self.carga="superior"
+    def __init__(self,nombre,fabricante,precio,carga):
+        super().__init__(nombre,fabricante,precio)
+        self.nombre=nombre
+        self.fabricante=fabricante
+        self.precio=precio
+        self.carga=carga
         print(self.nombre,self.fabricante,self.precio,self.consumo,self.carga)
         
 
 class Frigorifico(Electrodomestico):
-    def __init__(self):
-        super().__init__()
-        self.nombre="wrfasdf"
-        self.fabricante="Balay"
-        self.precio="400"
-        self.tipo="NoFrost"
+    def __init__(self,nombre,fabricante,precio,tipo):
+        super().__init__(nombre,fabricante,precio)
+        self.nombre=nombre
+        self.fabricante=fabricante
+        self.precio=precio
+        self.tipo=tipo
         print(self.nombre,self.fabricante,self.precio,self.consumo,self.tipo)
 
 class Freidora(Electrodomestico):
-    def __init__(self):
-        super().__init__()
-        self.nombre="wefrwer"
-        self.fabricante="Cecotec"
-        self.precio="50"
-        self.tem_max="200ºC"
+    def __init__(self,nombre,fabricante,precio,temp_max):
+        super().__init__(nombre,fabricante,precio)
+        self.nombre=nombre
+        self.fabricante=fabricante
+        self.precio=precio
+        self.tem_max=temp_max
         print(self.nombre,self.fabricante,self.precio,self.consumo,self.tem_max)
 
 
-portatil1=Portatil()
+portatil1=Portatil("qweq","HP",1800,"32GB")
 #portatil1.impri()
-teclado1=Teclado()
-monitor1=Monitores()
-lavadora1=Lavadora()
-frigorifico1=Frigorifico()
-freidora1=Freidora()
+teclado1=Teclado("qwjkde","logitec",90,"ergonomico")
+monitor1=Monitores("qwqwe","Asus",200,"21 Pulgadas")
+lavadora1=Lavadora("sadbh","Balay",300,"Carga superior")
+#frigorifico1=Frigorifico()
+#freidora1=Freidora()

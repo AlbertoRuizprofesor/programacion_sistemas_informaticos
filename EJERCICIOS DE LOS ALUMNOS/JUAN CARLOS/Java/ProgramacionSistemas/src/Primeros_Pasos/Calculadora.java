723cd6b3b4f79214996de65cae619d1b4079904f
Juan Carlos <jcs0969@icloud.com>
2026-02-02 14:12:36 +0100

    Create Calculadora.java

---
+++ EJERCICIOS DE LOS ALUMNOS/JUAN CARLOS/Java/ProgramacionSistemas/src/Primeros_Pasos/Calculadora.java
@@ -0,0 +1,30 @@
+package Primeros_Pasos;
+
+import java.util.Scanner;
+
+public class Calculadora {
+    public static void main (String[] args){
+        Scanner teclado = new Scanner(System.in);
+        int numero1, numero2;
+        int suma, resta, multiplicacion;
+        double division;
+        System.out.println("Introduce el primer valor: ");
+        numero1 = teclado.nextInt();
+        System.out.println("Introduce el segundo valor: ");
+        numero2 = teclado.nextInt();
+        suma = numero1 + numero2;
+        resta = numero1 - numero2;
+        multiplicacion = numero1 * numero2;
+        if (numero2 == 0) {
+            division = 0;
+        }else{
+                division = numero1 / numero2;
+        }
+        System.out.println("La suma de " + numero1 + " y " + numero2 + " es: " + suma);
+        System.out.println("La resta de " + numero1 + " y " + numero2 + " es: " + resta);
+        System.out.println("La multiplicacion de " + numero1 + " y " + numero2 + " es: " + multiplicacion);
+        System.out.println("La division de " + numero1 + " y " + numero2 + " es: " + division);
+        teclado.close();
+
+    }
+}

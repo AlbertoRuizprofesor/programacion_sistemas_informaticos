0066a2ba57d040890761e23271618e8faa450c1d
Mauri-74 <asarahlobo@gmail.com>
2026-03-17 11:16:21 +0100

    ejercicios

---
+++ EJERCICIOS DE LOS ALUMNOS/MAURICIO/PythonRepaso/Bloque_6_BD_SQLite/Ejercicio22_Consulta.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+conexion = sqlite3.connect("academia.db")
+cursor = conexion.cursor()
+
+cursor.execute("SELECT * FROM alumnos")
+print(cursor.fetchall())
+
+cursor.execute("SELECT nombre, nota FROM alumnos WHERE nota >= 8 ORDER BY nota DESC")
+print(cursor.fetchall())
+
+cursor.execute("SELECT AVG(nota) FROM alumnos")
+print("Nota media:", cursor.fetchone()[0])
+
+conexion.close()

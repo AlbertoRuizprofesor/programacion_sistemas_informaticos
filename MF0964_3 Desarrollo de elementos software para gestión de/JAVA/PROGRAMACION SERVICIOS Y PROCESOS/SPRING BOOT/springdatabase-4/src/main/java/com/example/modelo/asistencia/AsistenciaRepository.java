55f1008ff97b8a3c3ab63d00f841c9333675e056
JonAvilaE <jonavilae@gmail.com>
2026-03-18 08:39:18 +0100

    jlñ´lñ

--- MF0964_3 Desarrollo de elementos software para gestión de/JAVA/PROGRAMACION SERVICIOS Y PROCESOS/SPRING BOOT/springdatabase-4/src/main/java/com/example/modelo/asistencia/AsistenciaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.modelo.asistencia;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AsistenciaRepository extends JpaRepository<Asistencia, Integer> {
-}
--- MF0964_3 Desarrollo de elementos software para gestión de/JAVA/PROGRAMACION SERVICIOS Y PROCESOS/SPRING BOOT/springdatabase-4/src/main/java/com/example/modelo/profesores/ProfesorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.modelo.profesores;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProfesorRepository extends JpaRepository<Profesor, Integer> {
-}

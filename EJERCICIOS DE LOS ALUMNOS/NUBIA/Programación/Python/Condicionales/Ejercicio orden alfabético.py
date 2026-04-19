d045e7bd2aa96d4048272f70f1e7c5b9a3b324fa
Nyro <nyro0319@gmail.com>
2026-04-19 18:30:59 +0200

    Update Ejercicio orden alfabético.py

--- EJERCICIOS DE LOS ALUMNOS/NUBIA/Programación/Python/Condicionales/Ejercicio orden alfabético.py
+++ EJERCICIOS DE LOS ALUMNOS/NUBIA/Programación/Python/Condicionales/Ejercicio orden alfabético.py
@@ -5,3 +5,4 @@ if nombre1 > nombre2:
     print(f"{nombre1} va después que {nombre2} en orden alfabético.")
 elif nombre1 < nombre2:
     print(f"{nombre1} va antes que {nombre2} en orden alfabético.")
+
